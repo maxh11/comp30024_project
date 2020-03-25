0712838f11f055648d455bef46072ca7055ea731
maxh11 <max.hollands5253@gmail.com>
2020-03-25 20:59:32 +1100

    added heuristic (unfinished)

--- search/__main__.py
+++ search/__main__.py
@@ -19,7 +19,7 @@ def make_board_dict(data):
 
 
 def get_square_data(data, x, y):
-    # return string of square contents from coordinates (x, y)
+    # return string of square contents (e.g. '4b') from coordinates x, y
     # e.g. if a square at (1, 3) is occupied by 3 white pieces, get_square_data(data, 1, 3) returns '3w'
     # return '' if nothing in the given coordinates
     for colour in data:
@@ -31,6 +31,20 @@ def get_square_data(data, x, y):
     return ''
 
 
+def make_state_dict(data, colour):
+    # input is json data and a colour 'w' or 'b', for white or black
+    # return a dict of stack location coordinates as a key with n_pieces as the value
+    # a return example is: {(3, 2): 1, (0, 1): 5, (0, 2): 6}
+    stacks = {}
+    for x in range(BOARD_SIZE):
+        for y in range(BOARD_SIZE):
+            square = get_square_data(data, x, y)
+            if len(square) > 0:  # not an empty square
+                if colour in square:
+                    # just add the number of pieces in the stack as value
+                    stacks[(x, y)] = int(square[:-1])
+    return stacks
+
 
 def main():
     with open(sys.argv[1]) as file:
@@ -38,11 +52,20 @@ def main():
 
     # TODO: find and print winning action sequence
     print_utils.print_board(make_board_dict(data))
-    init_state = ai.State()
-    current_node = ai.Node()
 
+    # make the initial state and the initial node
+    init_state = ai.State(make_state_dict(data, WHITE_PIECE), make_state_dict(data, BLACK_PIECE))
+    current_node = ai.Node(init_state)
 
-    while ai.get_next_move()
+    # run ai and start generating winning moves
+    moves_made = []  # keep track of the moves we have made
+    current_node = ai.get_next_move(current_node)
+    while current_node is not None:
+        moves_made.append(current_node.move_made)
+        current_node = ai.get_next_move(current_node)
+
+    # print the moves made
+    print(moves_made)
 
 if __name__ == '__main__':
     main()
--- search/ai.py
+++ search/ai.py
@@ -1,4 +1,5 @@
 import heapq
+import sys
 
 LEFT = (-1, 0)
 RIGHT = (1, 0)
@@ -6,62 +7,83 @@ UP = (0, 1)
 DOWN = (0, -1)
 MOVE_DIRECTIONS = [LEFT, RIGHT, UP, DOWN]
 
+# values for the heuristic
+LOST_GAME = sys.maxsize
+WIN_GAME = -sys.maxsize
 
 
 class Node:
-    def __init__(self, state=None, parent=None, move=None, depth=None):
+    def __init__(self, state, parent=None, move=None, depth=0, h=0, f=0):
         """Node class for searching with A*"""
-        self.parent = parent
-        self.state = state
-        self.move = move
-        self.depth = depth
 
-        self.g = 0
-        self.h = 0
-        self.f = 0
+        # the state associated with this node
+        self.state = state
+        # the parent of this node, no parent if it was an initial node
+        self.parent = parent
+        # the last move that got us to this state in the form (stack_location, is_boom_action, n_pieces, move_direction, n_steps)
+        self.move = move
+        # the depth of the current node
+        self.depth = depth
+        # a* value: f(n) = h(n)<-heuristic + g(n)<-depth
+        self.h = heuristic(state)
+        self.f = h + depth
 
 class State:
     """State class to be associated with 1 node"""
-    def __init__(self, white_stacks, total_white_pieces, black_stacks, total_black_pieces):
-        self.white_stacks
+    def __init__(self, white_stacks, black_stacks):
 
+        # eg. white_stacks = {(3, 2): 1, (0, 1): 5, (0, 2): 6}
+        self.white_stacks = white_stacks
+        self.total_white = sum(white_stacks.values())
+        self.black_stacks = black_stacks
+        self.total_black = sum(black_stacks.values())
 
+def heuristic(state):
+    if state.total_black == 0:
+        return WIN_GAME
+    if state.total_white == 0:
+        return LOST_GAME
+    # else, the heuristic is the number of enemy pieces on the board, lower is better
+    return state.total_black
 
-def get_next_move(init_state, budget=100):
-
-    # make the start node
-    start_node = Node(None, init_state)
+def get_next_move(start_node, budget=100):
 
     # make a list of all the nodes we explore
     explored_nodes = []
 
     # make the frontier priority queue with only the start node
     frontier = []
-    heapq.heappush(frontier, (0, init_state))
+    heapq.heappush(frontier, (0, start_node))
+
+    # each action takes the form: (score, (stack_location, is_boom_action, n_pieces, move_direction, n_steps))
+    # score will be the number of enemy pieces left on board, ties broken randomly
+    best_action = ()
 
     # while there are still nodes in the frontier and we're not over budget, expand more
     while len(frontier) > 0 and len(explored_nodes) <= budget:
-        current_node = heapq.heappop(frontier)
+        current_node = heapq.heappop(frontier)[1]
         explored_nodes += [current_node]
 
+        # go through the list of applicable boom actions and try them
+
         # go through the list of applicable move actions and try them
-        i = 0 # index of the stack we're analysing
         # go through the squares where we have a stack
-        for stack in current_node.state.stack_locations:
+        for stack in current_node.state.white_stacks.items():
             # iterate through each possible number of pieces to move from our stack at the current occupied_square
-            for n_pieces in range(1, current_node.state.stack_sizes[i] + 1):
+            for n_pieces in range(1, stack[1] + 1):
                 # possible moving directions
                 for move_direction in MOVE_DIRECTIONS:
-                    # number of squares to move n_pieces from current stack, 1 <= n_steps <= stack
-                    for n_steps in range(1, current_node.state.stack_sizes[i] + 1):
+                    # number of squares to move n_pieces from current stack, 1 <= n_steps <= n_pieces
+                    for n_steps in range(1, stack[1] + 1):
                         # check if moving n_steps in move_direction from current stack is a legal move (i.e. not out of bounds and not landing on an enemy piece)
-                        if is_legal_move(current_node.state.enemy_stack_locations, stack, move_direction, n_steps):
+                        if is_legal_move(current_node.state.black_stacks, stack[0], move_direction, n_steps):
                             # make a child node that is the result of applying this move action to the current_node
                             child_node = apply_action(current_node, stack, n_pieces, move_direction, n_steps)
-
-
-            i += 1 # check to next stack
-
+                            # make sure we're not duplicating states
+                            if child_node not in explored_nodes:
+                                explored_nodes += [child_node]
+                                heapq.heappush(frontier, (child_node.f, child_node))
+                                if
 
 def is_legal_move(enemy_stack_locations, moving_stack_location, move_direction, n_steps):
     """ check if moving n_steps in move_direction from current stack is a legal move (i.e. not out of bounds and not landing on an enemy piece)"""
@@ -90,9 +112,9 @@ def apply_action(base_node, stack, n_pieces, move_direction, n_steps):
     stack_index = base_node.state.stack_locations.index(stack)
     # check if we moved the whole stack, remove square coords from stack_locations in that case
     if base_node.state.stack_quantities[stack_index] == n_pieces:
-        base_node.state.stack_quantities.pop(stack_index)
+        base_node.state..pop(stack_index)
         base_node.state.stack_locations.pop(stack_index)
 
-
+def apply_action(base_node, stack)
+    """overloaded function for boom actions, return a new node """
     return ...
-def boom_action(base_node)
